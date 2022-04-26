2cc65db96731cba6a5ccca6c18fc97e8dc068ba3
小力王 <6155364@qq.com>
2022-04-26 15:37:50 +0800

    nacos

--- xdclass-order-service/src/main/java/net/xdclass/OrderApplication.java
+++ xdclass-order-service/src/main/java/net/xdclass/OrderApplication.java
@@ -2,6 +2,7 @@ package net.xdclass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
  **/
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderApplication {
 
     public static void main(String [] args){
--- xdclass-video-service/src/main/java/net/xdclass/VideoApplication.java
+++ xdclass-video-service/src/main/java/net/xdclass/VideoApplication.java
@@ -3,15 +3,21 @@ package net.xdclass;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Description 旭瑶&小滴课堂 xdclass.net
+ * 什么是注册中心（服务治理）
+ * 服务注册：服务提供者provider，启动的时候向注册中心上报自己的网络信息
+ * 服务发现：服务消费者consumer,启动的时候向注册中心上报自己的网络信息，拉取provider的相关网络信息
+ * 核心:服务管理,是有个服务注册表，心跳机制动态维护，服务实例在启动时注册到服务注册表，并在关闭时注销。
  * @Author 二当家小D  代码、笔记和技术指导联系我即可
  * @Version 1.0
  **/
 
 @SpringBootApplication
 @MapperScan("net.xdclass.dao")
+@EnableDiscoveryClient
 public class VideoApplication {
 
     public static void main(String [] args){
